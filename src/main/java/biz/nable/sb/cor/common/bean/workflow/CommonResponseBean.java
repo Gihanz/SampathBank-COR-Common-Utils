1cd9c5bd9af63e43316558d995c90cf5db063671
Shehan Thamoda <thamodashehan92@gmail.com>
2020-04-08 10:36:08 +0530

    add same CommonResponse to workflow part

--- src/main/java/biz/nable/sb/cor/common/bean/workflow/CommonResponseBean.java
+++ src/main/java/biz/nable/sb/cor/common/bean/workflow/CommonResponseBean.java
@@ -1,11 +1,12 @@
 package biz.nable.sb.cor.common.bean.workflow;
 
+import biz.nable.sb.cor.common.response.CommonResponse;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class CommonResponseBean {
+public class CommonResponseBean extends CommonResponse {
 	private CommonTempBean commonTempBean;
 	private String tempId;
 	private String workflowId;
